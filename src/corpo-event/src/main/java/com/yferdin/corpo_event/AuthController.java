288251e26862b2701c84208ed5b26076e13dc4ba
Yohan Ferdin <yohan.ferdin@hotmail.fr>
2024-12-25 18:18:59 +0100

    feat(AuthController.java): update routing

--- src/corpo-event/src/main/java/com/yferdin/corpo_event/AuthController.java
+++ src/corpo-event/src/main/java/com/yferdin/corpo_event/AuthController.java
@@ -2,28 +2,30 @@ package com.yferdin.corpo_event;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import jakarta.servlet.http.HttpSession;
 @Controller
-@RequestMapping("/auth")
+@RequestMapping("/")
 public class AuthController {
     @Autowired
     private AuthService authService;
-    @GetMapping("/login")
+    @GetMapping("/")
     private String login() {
     	return "login";
     }
-    @PostMapping("/login")
-    public String login(@RequestParam String email, @RequestParam String password, HttpSession session) {
+    @PostMapping("/")
+    public String login(@RequestParam String email, @RequestParam String password, HttpSession session, RedirectAttributes redirAttr) {
         boolean success = authService.login(email, password);
         if (success) {
         	User user = authService.getUser(email);
         	session.setAttribute("userMail", email);
         	session.setAttribute("userRole", user.getRole());
-            return "redirect:/";
+            return "redirect:/home/";
         } else {
-        	System.out.println("not ok");
-            return "denied";
+        	redirAttr.addFlashAttribute("error", "Une erreur est survenue");
+            return "redirect:/";
         }
     }
 }
