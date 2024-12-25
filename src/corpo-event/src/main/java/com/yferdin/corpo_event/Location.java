c1ff05d5535dfb7e03d3062bae167791fc78404a
Yohan Ferdin <yohan.ferdin@hotmail.fr>
2024-12-25 11:54:37 +0100

    feat(Location.java): add location entity

--- src/corpo-event/src/main/java/com/yferdin/corpo_event/Location.java
+++ src/corpo-event/src/main/java/com/yferdin/corpo_event/Location.java
@@ -1,9 +1,14 @@
-package model;
+package com.yferdin.corpo_event;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.UUID;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "locations")
 public class Location {
     @Id
