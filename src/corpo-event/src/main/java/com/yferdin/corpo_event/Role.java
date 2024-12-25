7860451a8f7ca2d3983d4d8c6826a234630193fe
Yohan Ferdin <yohan.ferdin@hotmail.fr>
2024-12-25 11:54:51 +0100

    feat(Roles.java): add roles entity

--- src/corpo-event/src/main/java/com/yferdin/corpo_event/Role.java
+++ src/corpo-event/src/main/java/com/yferdin/corpo_event/Role.java
@@ -1,9 +1,14 @@
-package model;
+package com.yferdin.corpo_event;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.UUID;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "roles")
 public class Role {
     @Id
@@ -12,6 +17,11 @@ public class Role {
 
     @Column(name = "role_name", nullable = false)
     private String roleName;
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return roleName;
+	}
 
 }
 
