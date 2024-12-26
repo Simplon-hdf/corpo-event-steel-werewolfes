19f7e1a0b06eefa4eff9d31a43b1814859a17c56
Yohan Ferdin <yohan.ferdin@hotmail.fr>
2024-12-26 21:51:28 +0100

    feat(EventController.java): add create event form and struc of create event post reply

---
+++ src/corpo-event/src/main/java/com/yferdin/corpo_event/EventController.java
@@ -0,0 +1,37 @@
+package com.yferdin.corpo_event;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class EventController{
+	
+@Autowired	
+private EventRepository eventRepository;
+
+@Autowired
+private LocationRepository locationRepository;
+
+@Autowired
+private UserRepository userRepository;
+
+@GetMapping("/event/new")
+public String displayEventForm(Model model) {
+	model.addAttribute("event", new Event());
+	return "newEvent";
+}
+
+@PostMapping("/event/new")
+public String persistEvent(@ModelAttribute Event event, HttpServletRequest request) {
+	String mail = request.getSession().getAttribute("userMail").toString();
+	
+	return "of-the-jedi";
+}
+}
