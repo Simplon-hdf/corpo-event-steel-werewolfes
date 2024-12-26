121461c53631b880767049f6dcd0384e307c759c
Yohan Ferdin <yohan.ferdin@hotmail.fr>
2024-12-26 21:53:45 +0100

    feat(HomeController.java): edit home controller - pass event to view

--- src/corpo-event/src/main/java/com/yferdin/corpo_event/HomeController.java
+++ src/corpo-event/src/main/java/com/yferdin/corpo_event/HomeController.java
@@ -1,14 +1,23 @@
 package com.yferdin.corpo_event;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/home")
 public class HomeController {
+	@Autowired
+	EventRepository eventRepo;
+	
 	@GetMapping("/")
-	public String showHome() {
+	public String showHome(Model model) {
+		List<Event> events = eventRepo.findAll();
+		model.addAttribute("events", events);
 		return"home";
 	}
 }
