9ee689956070afa1b140822123e7bb7963115173
Yohan Ferdin <yohan.ferdin@hotmail.fr>
2024-12-28 21:32:48 +0100

    feat(User.java): add getters and setters

--- src/corpo-event/src/main/java/com/yferdin/corpo_event/User.java
+++ src/corpo-event/src/main/java/com/yferdin/corpo_event/User.java
@@ -24,7 +24,43 @@ public class User {
     @Column(name = "pass", nullable = false)
     private String password;
 
-    @Column(name = "mail", nullable = false, unique = true)
+    public UUID getUserId() {
+		return userId;
+	}
+
+	public void setUserId(UUID userId) {
+		this.userId = userId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	@Column(name = "mail", nullable = false, unique = true)
     private String email;
 
     @ManyToOne
